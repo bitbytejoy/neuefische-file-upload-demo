3630dc26b7aa8c365553ffacec2613e775570241
bitbytejoy <bitbytejoy@gmail.com>
2023-01-24 13:05:55 +0100

    init: BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = React.useState<File | null>(null);
+  const [imgPreview, setImgPreview] = React.useState<string | null>(null);
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {imgPreview && (
+        <img
+          src={imgPreview}
+          alt={"preview"}
+        />
+      )}
+
+      <form onSubmit={async (e) => {
+        // FILE UPLOAD
+        e.preventDefault();
+
+        if (file) {
+          const formData = new FormData();
+          formData.append("file", file);
+
+          const res = await axios.post("/api/files", formData);
+
+          alert(JSON.stringify(res.data, null, 2));
+        }
+      }}>
+        <button onClick={() => {
+          fileInputRef.current?.click();
+        }}> UPLOAD IMAGE</button>
+
+        <input
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          type={"file"}
+          onChange={(e) => {
+            // FILE CHANGE
+            if (!e.target.files || e.target.files.length < 1) {
+              setFile(null);
+              setImgPreview(null);
+              return;
+            }
+
+            setFile(e.target.files[0]);
+
+            // PREVIEW
+
+            const reader = new FileReader();
+
+            reader.addEventListener("load", () => {
+              // convert image file to base64 string
+              setImgPreview(reader.result as string);
+            }, false);
+
+            if (file) {
+              reader.readAsDataURL(file);
+            }
+          }}
+          accept={"image/png"}
+        />
+
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
